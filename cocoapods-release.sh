7689856635083ccd1e4d7a9b2dfc6255f4a94909
SamuelOsborne <sam@lottiefiles.com>
2026-05-27 10:32:27 +0200

    chore: small cocoapods build refactor

--- cocoapods-release.sh
+++ cocoapods-release.sh
@@ -1,3 +1,6 @@
+# Run before releasing to cocoapods. This builds a seperate xcframework for cocoapods that disregards watchOS due to lack of cocoapods support for the system.
+# Won't run if Sources/DotLottieCore/cocoapods/DotLottiePlayer.xcframework is already present
+
 xcodebuild -create-xcframework \
     -framework Sources/DotLottieCore/DotLottiePlayer.xcframework/ios-arm64/DotLottiePlayer.framework \
     -framework Sources/DotLottieCore/DotLottiePlayer.xcframework/ios-arm64_x86_64-simulator/DotLottiePlayer.framework \
